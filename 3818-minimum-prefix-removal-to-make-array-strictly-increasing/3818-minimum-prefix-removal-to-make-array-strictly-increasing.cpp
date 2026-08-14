c55fd89d33676acfaf53ede88bf9ce404dc50f37
Kavya Jain <24ucs058@lnmiit.ac.in>
2026-08-15 03:30:50 +0530

    Time: 0 ms (100%), Space: 193.4 MB (51.6%) - LeetHub

---
+++ 3818-minimum-prefix-removal-to-make-array-strictly-increasing/3818-minimum-prefix-removal-to-make-array-strictly-increasing.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minimumPrefixLength(vector<int>& nums) {
+        int i;
+        for(i = nums.size() - 1; i >= 1; i--){
+            if(nums[i] <= nums[i-1]) break;
+        }
+
+        return i;
+    }
+};

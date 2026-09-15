940e8ac314ddafd7e7948a2fe26742cd424fc04d
Kavya Jain <24ucs058@lnmiit.ac.in>
2026-09-16 01:06:12 +0530

    Time: 13 ms (49.44%), Space: 32.3 MB (47.66%) - LeetHub

--- 4048-count-values-with-equally-spaced-occurrences-i/4048-count-values-with-equally-spaced-occurrences-i.cpp
+++ 4048-count-values-with-equally-spaced-occurrences-i/4048-count-values-with-equally-spaced-occurrences-i.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     int countSpecialIntegers(vector<int>& nums) {
         map<int, vector<int>> a;
-        for(int i = 0; i < nums.size(); i++){
-            a[nums[i]].push_back(i);
-        }
+        for(int i = 0; i < nums.size(); i++) a[nums[i]].push_back(i);
 
         int count = 0;
         for(auto& p : a){

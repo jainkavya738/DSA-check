2729c099358e171f55c2657bb645794fd0135774
Kavya Jain <24ucs058@lnmiit.ac.in>
2026-09-23 18:46:53 +0530

    Time: 0 ms (100%), Space: 13.4 MB (35.88%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans = 0;
-        for(int bitIdx = 0; bitIdx < 32; bitIdx++){
-            int count1 = 0;
-            for(int i = 0; i < nums.size(); i++){
-                if(nums[i] & (1 << bitIdx)) count1++;
-            }
-
-            if(count1%3 == 1) ans |= (1 << bitIdx);
+        int ones = 0, twos = 0;
+        for(int i = 0; i < nums.size(); i++){
+            ones = (ones ^ nums[i]) & ~twos;
+            twos = (twos ^ nums[i]) & ~ones;
         }
 
-        return ans;
+        return ones;
     }
 };
+
+// class Solution {
+// public:
+//     int singleNumber(vector<int>& nums) {
+//         int ans = 0;
+//         for(int bitIdx = 0; bitIdx < 32; bitIdx++){
+//             int count1 = 0;
+//             for(int i = 0; i < nums.size(); i++){
+//                 if(nums[i] & (1 << bitIdx)) count1++;
+//             }
+
+//             if(count1%3 == 1) ans |= (1 << bitIdx);
+//         }
+
+//         return ans;
+//     }
+// };
